df2c9f5bf63ae78fa27b14f542e15361193cc957
gvln-s <santis.paez2414@outlook.com>
2026-03-02 15:38:09 -0500

    fix: :bug: delete isodir dir after comp

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rm -rf isodir/
-
 i686-elf-as boot.s -o ./output/boot.o
 i686-elf-gcc -c kernel.c -o ./output/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T linker.ld -o ./output/gvln-os -ffreestanding -O2 -nostdlib ./output/boot.o ./output/kernel.o -lgcc
@@ -9,6 +7,8 @@ i686-elf-gcc -T linker.ld -o ./output/gvln-os -ffreestanding -O2 -nostdlib ./out
 grub-file --is-x86-multiboot ./output/gvln-os 
 
 mkdir -p isodir/boot/grub
-cp gvln-os isodir/boot/gvln-os
+cp ./output/gvln-os isodir/boot/gvln-os
 cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o gvln-os.iso isodir
+grub-mkrescue -o ./output/gvln-os.iso isodir
+
+rm -rf isodir
